17b0aae8ae4f933b4ce12173e56255508a7ff1c5
JayTheThird <jaypatel161200@gmail.com>
2024-07-17 11:40:43 +0530

    create urban outfitters repo created login, sign up and home page with style, js and images

---
+++ pages/index.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Urban Outfitter's</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mukta:300,400,700">
+    <link rel="stylesheet" href="../fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="../styles/bootstrap.min.css">
+    <link rel="stylesheet" href="../styles/magnific-popup.css">
+    <link rel="stylesheet" href="../styles/jquery-ui.css">
+    <link rel="stylesheet" href="../styles/owl.carousel.min.css">
+    <link rel="stylesheet" href="../styles/owl.theme.default.min.css">
+
+
+    <link rel="stylesheet" href="../styles/aos.css">
+
+    <link rel="stylesheet" href="../styles/style.css">
+
+</head>
+
+<body>
+    <div class="site-wrap">
+        <!-- Header -->
+        <header class="site-navbar" role="banner">
+            <div class="site-navbar-top">
+                <div class="container">
+                    <div class="row align-items-center">
+
+                        <div class="col-6 col-md-4 order-2 order-md-1 site-search-icon text-left">
+                            <form action="" class="site-block-top-search">
+                                <span class="icon icon-search2"></span>
+                                <input type="text" class="form-control border-0" placeholder="Search">
+                            </form>
+                        </div>
+
+                        <div class="col-12 mb-3 mb-md-0 col-md-4 order-1 order-md-2 text-center">
+                            <div class="site-logo">
+                                <a href="index.php" class="js-logo-clone">Urban Outfitter's</a>
+                            </div>
+                        </div>
+
+                        <div class="col-6 col-md-4 order-3 order-md-3 text-right">
+                            <div class="site-top-icons">
+                                <ul>
+                                    <li><a href="#"><span class="icon icon-person"></span></a></li>
+                                    <li><a href="#"><span class="icon icon-heart-o"></span></a></li>
+                                    <li>
+                                        <a href="cart.html" class="site-cart">
+                                            <span class="icon icon-shopping_cart"></span>
+                                            <span class="count">0</span>
+                                        </a>
+                                    </li>
+                                    <li class="d-inline-block d-md-none ml-md-0"><a href="#" class="site-menu-toggle js-menu-toggle"><span class="icon-menu"></span></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <nav class="site-navigation text-right text-md-center" role="navigation">
+                <div class="container">
+                    <ul class="site-menu js-clone-nav d-none d-md-block">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li class="has-children">
+                            <a href="">Category</a>
+                            <ul class="dropdown">
+                                <li><a href="#categories">Urban Jeans</a></li>
+                                <li><a href="#categories">Urban Shirts</a></li>
+                                <li><a href="#categories">Urban T Shirts</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#big-sale">Big Sale!</a></li>
+                        <li><a href="shop.html">Shop</a></li>
+                        <li><a href="#">Catalogue</a></li>
+                        <li><a href="#featured-products">New Arrivals</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!--  -->
+
+        <!-- hero section -->
+        <div class="site-blocks-cover" style="background-image: url(../images/banner/main-banner.png);" data-aos="fade">
+            <div class="container">
+                <div class="row align-items-start align-items-md-center justify-content-end">
+                    <div class="col-md-5 text-center text-md-left pt-5 pt-md-0">
+                        <h1 class="mb-1">Finding Your Perfect Cloths</h1>
+                        <div class="intro-text text-center text-md-left">
+                            <p class="mb-2">Style is something each of us already has, all we need to do is find it. </p>
+                            <p>
+                                <a href="#" class="btn btn-sm btn-primary">Shop Now</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--  -->
+
+        <!-- details -->
+        <div class="site-section site-section-sm site-blocks-1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="">
+                        <div class="icon mr-4 align-self-start">
+                            <span class="icon-clock-o"></span>
+                        </div>
+                        <div class="text">
+                            <h2 class="text-uppercase">Free Shipping</h2>
+                            <p>Enjoy free shipping on all orders across India. Quick and safe delivery to your doorstep, without any hidden charges.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="100">
+                        <div class="icon mr-4 align-self-start">
+                            <span class="icon-rotate-right"></span>
+                        </div>
+                        <div class="text">
+                            <h2 class="text-uppercase">Easy Returns</h2>
+                            <p>Return within 3 days for a full refund or exchange. Simple, hassle-free, and no questions asked.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-4 d-lg-flex mb-4 mb-lg-0 pl-4" data-aos="fade-up" data-aos-delay="200">
+                        <div class="icon mr-4 align-self-start">
+                            <span class="icon-headset"></span>
+                        </div>
+                        <div class="text">
+                            <h2 class="text-uppercase">Customer Support</h2>
+                            <p>24/7 customer support via phone, email, or live chat. We're here to help you anytime.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--  -->
+
+    <!-- Categories -->
+    <div class="site-section site-blocks-2" id="categories">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
+                    <a class="block-2-item" href="#">
+                        <figure class="image">
+                            <img src="../images/categories/jeans-category.jpg" alt="" class="img-fluid">
+                        </figure>
+                        <div class="text">
+                            <span class="text-uppercase">Collections</span>
+                            <h3>Jeans</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
+                    <a class="block-2-item" href="#">
+                        <figure class="image">
+                            <img src="../images/categories/shirts-category.jpg" alt="" class="img-fluid">
+                        </figure>
+                        <div class="text">
+                            <span class="text-uppercase">Collections</span>
+                            <h3>Shirts</h3>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
+                    <a class="block-2-item" href="#">
+                        <figure class="image">
+                            <img src="../images/categories/tshirts-category.jpg" alt="" class="img-fluid">
+                        </figure>
+                        <div class="text">
+                            <span class="text-uppercase">Collections</span>
+                            <h3>T Shirts</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- features products -->
+    <div class="site-section block-3 site-blocks-2 bg-light" id="featured-products">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-7 site-section-heading text-center pt-4">
+                    <h2>Featured Products</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="nonloop-block-3 owl-carousel">
+                        <div class="item">
+                            <div class="block-4 text-center">
+                                <figure class="block-4-image">
+                                    <img src="../images/products/f1.jpg" alt="Image placeholder" class="img-fluid">
+                                </figure>
+                                <div class="block-4-text p-4">
+                                    <h3><a href="#">Tank Top</a></h3>
+                                    <p class="mb-0">Finding perfect t-shirt</p>
+                                    <p class="text-primary font-weight-bold">$50</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="block-4 text-center">
+                                <figure class="block-4-image">
+                                    <img src="../images/products/f6.jpg" alt="Image placeholder" class="img-fluid">
+                                </figure>
+                                <div class="block-4-text p-4">
+                                    <h3><a href="#">Corater</a></h3>
+                                    <p class="mb-0">Finding perfect products</p>
+                                    <p class="text-primary font-weight-bold">$50</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="block-4 text-center">
+                                <figure class="block-4-image">
+                                    <img src="../images/products/n1.jpg" alt="Image placeholder" class="img-fluid">
+                                </figure>
+                                <div class="block-4-text p-4">
+                                    <h3><a href="#">Polo Shirt</a></h3>
+                                    <p class="mb-0">Finding perfect products</p>
+                                    <p class="text-primary font-weight-bold">$50</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="block-4 text-center">
+                                <figure class="block-4-image">
+                                    <img src="../images/products/n8.jpg" alt="Image placeholder" class="img-fluid">
+                                </figure>
+                                <div class="block-4-text p-4">
+                                    <h3><a href="#">T-Shirt Mockup</a></h3>
+                                    <p class="mb-0">Finding perfect products</p>
+                                    <p class="text-primary font-weight-bold">$50</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="block-4 text-center">
+                                <figure class="block-4-image">
+                                    <img src="../images/shoe_1.jpg" alt="Image placeholder" class="img-fluid">
+                                </figure>
+                                <div class="block-4-text p-4">
+                                    <h3><a href="#">Corater</a></h3>
+                                    <p class="mb-0">Finding perfect products</p>
+                                    <p class="text-primary font-weight-bold">$50</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--  -->
+
+
+    <!-- Big Sale! -->
+    <div class="site-section block-8" id="big-sale">
+        <div class="container">
+            <div class="row justify-content-center  mb-5">
+                <div class="col-md-7 site-section-heading text-center pt-4">
+                    <h2>Big Sale!</h2>
+                </div>
+            </div>
+            <div class="row align-items-center">
+                <div class="col-md-12 col-lg-7 mb-5">
+                    <a href="#"><img src="../images/blog_1.jpg" alt="Image placeholder" class="img-fluid rounded"></a>
+                </div>
+                <div class="col-md-12 col-lg-5 text-center pl-md-5">
+                    <h2><a href="#">50% less in all items</a></h2>
+                    <p class="post-meta mb-4">By <a href="#">Carl Smith</a> <span class="block-8-sep">&bullet;</span> September 3, 2018</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam iste dolor accusantium facere corporis ipsum animi deleniti fugiat. Ex, veniam?</p>
+                    <p><a href="#" class="btn btn-primary btn-sm">Shop Now</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--  -->
+
+    <!-- footers -->
+    <footer class="site-footer border-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 mb-5 mb-lg-0">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h3 class="footer-heading mb-4 text-primary">Urban Outfitter's</h3>
+                        </div>
+                        <div class="col-md-6 col-lg-4">
+                            <ul class="list-unstyled">
+                                <li><a href="#">Sell online</a></li>
+                                <li><a href="#">Features</a></li>
+                                <li><a href="#">Shopping cart</a></li>
+                                <li><a href="#">Store builder</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 col-lg-4">
+                            <ul class="list-unstyled">
+                                <li><a href="#">Mobile commerce</a></li>
+                                <li><a href="#">Dropshipping</a></li>
+                                <li><a href="#">Website development</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 col-lg-4">
+                            <ul class="list-unstyled">
+                                <li><a href="#">Point of sale</a></li>
+                                <li><a href="#">Hardware</a></li>
+                                <li><a href="#">Software</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-4 mb-lg-0">
+                    <h3 class="footer-heading mb-4">Promo</h3>
+                    <a href="#" class="block-6">
+                        <img src="../images/hero_1.jpg" alt="Image placeholder" class="img-fluid rounded mb-4">
+                        <h3 class="font-weight-light  mb-0">Finding Your Perfect Shoes</h3>
+                        <p>Promo from nuary 15 &mdash; 25, 2019</p>
+                    </a>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="block-5 mb-5">
+                        <h3 class="footer-heading mb-4">Contact Info</h3>
+                        <ul class="list-unstyled">
+                            <li class="address">203 Fake St. Mountain View, San Francisco, California, USA</li>
+                            <li class="phone"><a href="tel://23923929210">+2 392 3929 210</a></li>
+                            <li class="email">emailaddress@domain.com</li>
+                        </ul>
+                    </div>
+
+                    <div class="block-7">
+                        <form action="#" method="post">
+                            <label for="email_subscribe" class="footer-heading">Subscribe</label>
+                            <div class="form-group">
+                                <input type="text" class="form-control py-4" id="email_subscribe" placeholder="Email">
+                                <input type="submit" class="btn btn-sm btn-primary" value="Send">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="row pt-5 mt-5 text-center">
+                <div class="col-md-12">
+                    <p>
+
+                        Copyright &copy;<script data-cfasync="false" src="#"></script>
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | <a href="#" target="_blank" class="text-primary">Urban Outfitter's</a>
+
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+    </div>
+
+    <script src="../js/jquery-3.3.1.min.js"></script>
+    <script src="../js/jquery-ui.js"></script>
+    <script src="../js/popper.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/jquery.magnific-popup.min.js"></script>
+    <script src="../js/aos.js"></script>
+
+    <script src="../js/main.js"></script>
+
+</body>
+
+</html>
---
+++ pages/user_login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Registration or Sign Up form in HTML CSS | CodingLab </title>
+    <link rel="stylesheet" href="../styles/login_signup_style.css">
+
+</head>
+
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <form action="#">
+
+            <div class="input-box">
+                <input type="text" placeholder="Email" required>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <div class="policy">
+                <input type="checkbox">
+                <h3>I accept all terms & condition</h3>
+            </div>
+            <div class="input-box button">
+                <input type="Submit" value="Login">
+            </div>
+            <div class="text">
+                <h3>Don't Have an account? <a href="user_signup.php">Sign now</a></h3>
+            </div>
+        </form>
+    </div>
+
+</body>
+
+</html>
