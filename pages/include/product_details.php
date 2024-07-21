599626de8fd7a200f2d34debbf9c4c0a55d8b446
JayTheThird <jaypatel161200@gmail.com>
2024-07-21 10:42:41 +0530

    added product details page

---
+++ pages/include/product_details.php
@@ -0,0 +1,59 @@
+ <div class="site-section">
+     <div class="container">
+         <div class="row">
+
+             <!-- Product Image -->
+             <div class="col-md-6">
+                 <img src="../images/products/f1.jpg" alt="Tank Top T-Shirt" class="img-fluid">
+             </div>
+
+             <div class="col-md-6">
+                 <!-- Product Name -->
+                 <h2 class="text-black">Tank Top T-Shirt</h2>
+
+                 <!-- Product Description -->
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, vitae, explicabo? Incidunt facere, natus soluta dolores iusto! Molestiae expedita veritatis nesciunt doloremque sint asperiores fuga voluptas, distinctio, aperiam, ratione dolore.</p>
+                 <p class="mb-4">Ex numquam veritatis debitis minima quo error quam eos dolorum quidem perferendis. Quos repellat dignissimos minus, eveniet nam voluptatibus molestias omnis reiciendis perspiciatis illum hic magni iste, velit aperiam quis.</p>
+
+                 <!-- Product Price -->
+                 <p><strong class="text-primary h4">₹ 500</strong></p>
+
+                 <!-- Product Sizes -->
+                 <div class="mb-1 d-flex">
+                     <label class="d-flex mr-3 mb-3">
+                         <input type="radio" id="option-sm" name="shop-sizes" class="mr-2">
+                         <span class="d-inline-block text-black">Small</span>
+                     </label>
+                     <label class="d-flex mr-3 mb-3">
+                         <input type="radio" id="option-md" name="shop-sizes" class="mr-2">
+                         <span class="d-inline-block text-black">Medium</span>
+                     </label>
+                     <label class="d-flex mr-3 mb-3">
+                         <input type="radio" id="option-lg" name="shop-sizes" class="mr-2">
+                         <span class="d-inline-block text-black">Large</span>
+                     </label>
+                     <label class="d-flex mr-3 mb-3">
+                         <input type="radio" id="option-xl" name="shop-sizes" class="mr-2">
+                         <span class="d-inline-block text-black">Extra Large</span>
+                     </label>
+                 </div>
+
+                 <!-- Quantity Selector -->
+                 <div class="mb-5">
+                     <div class="input-group mb-3" style="max-width: 120px;">
+                         <div class="input-group-prepend">
+                             <button class="btn btn-outline-primary js-btn-minus" type="button">&minus;</button>
+                         </div>
+                         <input type="text" class="form-control text-center" value="1" placeholder="" aria-label="Quantity" aria-describedby="button-addon1">
+                         <div class="input-group-append">
+                             <button class="btn btn-outline-primary js-btn-plus" type="button">&plus;</button>
+                         </div>
+                     </div>
+                 </div>
+
+                 <!-- Add to Cart Button -->
+                 <p><a href="cart.html" class="buy-now btn btn-sm btn-primary">Add To Cart</a></p>
+             </div>
+         </div>
+     </div>
+ </div>
