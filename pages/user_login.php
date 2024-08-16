c5e44098a77c2abb7fe49addacb657d1a3136e7b
JayTheThird <jaypatel161200@gmail.com>
2024-08-16 18:44:21 +0530

    did some change on user_login and user_signup

--- pages/user_login.php
+++ pages/user_login.php
@@ -10,24 +10,24 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="../pages/admin/assets/img/favicon.png" rel="icon">
-    <link href="../pages/admin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="../admin/assets/img/favicon.png" rel="icon">
+    <link href="../admin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="../pages/admin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../pages/admin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="../pages/admin/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="../pages/admin/assets/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="../pages/admin/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="../pages/admin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="../pages/admin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <link href="../admin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../admin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../admin/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="../admin/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="../admin/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="../admin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="../admin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="../pages/admin/assets/css/style.css" rel="stylesheet">
+    <link href="../admin/assets/css/style.css" rel="stylesheet">
 
 
 </head>
@@ -41,21 +41,11 @@
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-                            <!-- title -->
-                            <div class="d-flex justify-content-center py-4">
-                                <a href="index.html" class="logo d-flex align-items-center w-auto">
-                                    <img src="../pages/admin/assets/img/logo.png" alt="">
-                                    <span class="d-none d-lg-block">Urban Outfitter's</span>
-                                </a>
-                            </div>
-                            <!--  -->
 
                             <!-- fields -->
                             <div class="card mb-3">
-
                                 <div class="card-body">
-
-                                    <div class="pt-4 pb-2">
+                                    <div class="pt-1 pb-1">
                                         <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
                                     </div>
 
@@ -108,17 +98,17 @@
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
-    <script src="../pages/admin/assets/vendor/apexcharts/apexcharts.min.js"></script>
-    <script src="../pages/admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="../pages/admin/assets/vendor/chart.js/chart.umd.js"></script>
-    <script src="../pages/admin/assets/vendor/echarts/echarts.min.js"></script>
-    <script src="../pages/admin/assets/vendor/quill/quill.js"></script>
-    <script src="../pages/admin/assets/vendor/simple-datatables/simple-datatables.js"></script>
-    <script src="../pages/admin/assets/vendor/tinymce/tinymce.min.js"></script>
-    <script src="../pages/admin/assets/vendor/php-email-form/validate.js"></script>
+    <script src="../admin/assets/vendor/apexcharts/apexcharts.min.js"></script>
+    <script src="../admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../admin/assets/vendor/chart.js/chart.umd.js"></script>
+    <script src="../admin/assets/vendor/echarts/echarts.min.js"></script>
+    <script src="../admin/assets/vendor/quill/quill.js"></script>
+    <script src="../admin/assets/vendor/simple-datatables/simple-datatables.js"></script>
+    <script src="../admin/assets/vendor/tinymce/tinymce.min.js"></script>
+    <script src="../admin/assets/vendor/php-email-form/validate.js"></script>
 
     <!-- Template Main JS File -->
-    <script src="../pages/admin/assets/js/main.js"></script>
+    <script src="../admin/assets/js/main.js"></script>
 
 </body>
 
