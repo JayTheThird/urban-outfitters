d4f806600696bfd5e5eca4971d3938e5cead23e3
JayTheThird <jaypatel161200@gmail.com>
2024-08-29 10:24:04 +0530

    displayed categories in users side

--- pages/include/categories.php
+++ pages/include/categories.php
@@ -1,39 +1,36 @@
-    <div class="site-section site-blocks-2" id="categories">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-6 col-lg-4 mb-4 mb-lg-0" data-aos="fade" data-aos-delay="">
-                    <a class="block-2-item" href="#">
-                        <figure class="image">
-                            <img src="../images/categories/jeans-category.jpg" alt="" class="img-fluid">
-                        </figure>
-                        <div class="text">
-                            <span class="text-uppercase">Collections</span>
-                            <h3>Jeans</h3>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="100">
-                    <a class="block-2-item" href="#">
-                        <figure class="image">
-                            <img src="../images/categories/shirts-category.jpg" alt="" class="img-fluid">
-                        </figure>
-                        <div class="text">
-                            <span class="text-uppercase">Collections</span>
-                            <h3>Shirts</h3>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-sm-6 col-md-6 col-lg-4 mb-5 mb-lg-0" data-aos="fade" data-aos-delay="200">
-                    <a class="block-2-item" href="#">
-                        <figure class="image">
-                            <img src="../images/categories/tshirts-category.jpg" alt="" class="img-fluid">
-                        </figure>
-                        <div class="text">
-                            <span class="text-uppercase">Collections</span>
-                            <h3>T Shirts</h3>
-                        </div>
-                    </a>
-                </div>
-            </div>
+<?php
+include_once("../connection.php");
+include_once("../config.php");
+?>
+
+
+<div class="site-section site-blocks-2" id="categories">
+    <div class="container">
+        <!-- Flex container for centering and wrapping items -->
+        <div style="display: flex; align-items: center; justify-content: space-evenly; flex-wrap: wrap;">
+            <?php
+            $category = "SELECT * FROM `product_sub_category`";
+            $category_query = mysqli_query($conn, $category);
+            if (mysqli_num_rows($category_query) > 0) {
+                while ($fetched_data = mysqli_fetch_assoc($category_query)) {
+            ?>
+                    <!-- Card for each category -->
+                    <div class="col-sm-12 col-md-6  col-lg-4 mb-5" style="display: flex; justify-content: center;">
+                        <a class="block-2-item" href="#" style="display: block; width: 100%; max-width: 300px;">
+                            <figure class="image" style="overflow: hidden; border-radius: 10px;">
+                                <!-- Corrected the image path -->
+                                <img src="../admin/uploaded_images/category/<?php echo $fetched_data['sub_category_image']; ?>" alt="category image not load" class="img-fluid" style="width: 100%; height: auto;">
+                            </figure>
+                            <div class="text" style="text-align: center; margin-top: 12px;">
+                                <span class="text-uppercase">Collections</span>
+                                <h3><?php echo $fetched_data['sub_category_name']; ?></h3>
+                            </div>
+                        </a>
+                    </div>
+            <?php
+                }
+            }
+            ?>
         </div>
     </div>
+</div>
